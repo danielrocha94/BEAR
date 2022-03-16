2bb7ab5c9a8a77a4886ef260ae18eee4d1a4debb
Daniel Rocha <danielrocha94@msn.com>
2022-03-16 17:32:28 -0600

    Initialize repository

---
+++ spec/bear_core_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe BearCore do
+  it "has a version number" do
+    expect(BearCore::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
